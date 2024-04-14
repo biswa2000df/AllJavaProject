53aee0c2502c9fcd12eb09d1ba4867444ec80771
biswajit <biswajitsahookanha11@gmail.com>
2024-04-14 16:14:45 +0530

    normal test

--- BrowserLunch.java
+++ BrowserLunch.java
@@ -1,8 +1,5 @@
 package PracticeCode;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class BrowserLunch {
 
     public static void main(String[] args) throws InterruptedException {
